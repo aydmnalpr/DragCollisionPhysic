6f3de68cc327ee3ada6e43265d5baed2e48328b3
Alper Ayduman <aydumanalper@gmail.com>
2021-01-06 16:01:09 +0300

    Initial commit

---
+++ Assets/DragObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    public Camera cam;
+    public Transform cube;
+    public float distanceFromCamera;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        distanceFromCamera = Vector3.Distance(cube.position, cam.transform.position);
+        rb = cube.GetComponent<Rigidbody>();
+    }
+
+    private Vector3 lastPos;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = distanceFromCamera;
+            pos = cam.ScreenToWorldPoint(pos);
+            //cube.position = new Vector3(pos.x,cube.position.y, pos.z);
+            rb.velocity = (pos - cube.position) * 10;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            rb.velocity = Vector3.zero;
+        }
+        
+        
+    }
+}
